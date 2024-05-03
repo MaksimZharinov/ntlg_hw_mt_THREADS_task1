75123958f735652606e57d58c25e1535aca439f4
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-05-04 02:14:18 +0700

    done this task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
 
@@ -7,6 +9,39 @@ public class Main {
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
+
+        long start = System.currentTimeMillis();
+        List<Thread> threads = new ArrayList<>();
+        for (String text : texts) {
+            threads.add(new Thread(() -> {
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
+                    }
+                }
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            }));
+            threads.getLast().start();
+        }
+        for (Thread thread : threads) {
+            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("Time with threads: " + (end - start) + "ms");
 
         long startTs = System.currentTimeMillis(); // start time
         for (String text : texts) {
